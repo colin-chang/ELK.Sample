f2211694e2cc07d2d912fe8ff97562e6e1907422
ColinChang <zhangcheng5468@gmail.com>
2019-04-30 11:25:53 +0800

    init samples

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace ConsoleSample
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            var servicesProvider = BuildDi();
+            var runner = servicesProvider.GetRequiredService<Runner>();
+            runner.DoAction("Action1");
+
+            logger.Debug("outer context test");
+            Console.WriteLine("Press ANY key to exit");
+            Console.ReadKey();
+            LogManager.Shutdown();
+        }
+
+        private static IServiceProvider BuildDi()
+        {
+            return new ServiceCollection()
+                .AddSingleton<Runner>() // Runner is the custom class
+                .AddLogging(loggingBuilder =>
+                {
+                    // configure Logging with NLog
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    loggingBuilder.AddNLog(new NLogProviderOptions {ParseMessageTemplates = true});
+                })
+                .BuildServiceProvider();
+        }
+    }
+
+    public class Runner
+    {
+        private readonly ILogger<Runner> _logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoAction(string name)
+        {
+            try
+            {
+                string a = null;
+                a.ToString();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "custom message");
+            }
+
+            _logger.LogWarning("test information log");
+        }
+    }
+}
---
+++ WebSample/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebSample.Models;
+
+namespace WebSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                string a = null;
+                a.ToString();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(new EventId(500, Request.GetDisplayUrl()), e, "custom message");
+            }
+            
+            _logger.LogWarning("test information log");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
